052609b5290da3dc71c32a8a43ceea2e0b872c01
Hafsa Qureshi <waseemhasnain373@gmail.com>
2023-12-09 18:06:53 +0500

    commit

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -2,7 +2,7 @@ import ban from '../assets/images/ban.png'
 export default function Banner(){
 return(
     <>
-    <div className="my-5 container justify-content-center text-center">
+    <div className="my-3 container justify-content-center text-center">
     <img src={ban} alt="" />
 
     </div>
